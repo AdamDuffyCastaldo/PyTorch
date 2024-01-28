8877df02c7f52a79bfd7eccb96aae8c5d215269b
Adam Duffy-Castaldo <adam.duffy-castaldo@challoners.org>
2024-01-28 22:57:57 +0000

    aea

---
+++ pytorch2.py
@@ -0,0 +1,9 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+
+import matplotlib.pyplot as plt
+%matplotlib inline
+from tqdm import tqdm
